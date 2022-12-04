9dd38da4a954323f02dc1c854c76d6727760ceac
Academy-of-Code <dillioncat77@gmail.com>
2022-12-04 10:26:30 -0500

    Completed Day 3

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -300,7 +300,7 @@ VsFfCzLvsMfzNfNRhVMslzlHqrWrQDQcqDqTrLWHcrWJJW
 dsRdsNCvNMVpwPdnnGbbPb'''
 
 values = {
-    "a": 1,"b": 2,"c": 3,"d": 4,"e": 5,"f": 6,"g": 7,"h": 8,"i": 9,"j": 10,"k": 11,"l": 12,"m": 13,"n": 14,"o": 15,"p": 16,"q":17,"r":18,"s":19,"t":20,"u":21,"v":22,"w":23,"x":24,"y":25,"z":26,
+    "a":  1,"b":  2,"c":  3,"d":  4,"e":  5,"f":  6,"g":  7,"h":  8,"i":  9,"j": 10,"k": 11,"l": 12,"m": 13,"n": 14,"o": 15,"p": 16,"q":17,"r":18,"s":19,"t":20,"u":21,"v":22,"w":23,"x":24,"y":25,"z":26,
     "A": 27,"B": 28,"C": 29,"D": 30,"E": 31,"F": 32,"G": 33,"H": 34,"I": 35,"J": 36,"K": 37,"L": 38,"M": 39,"N": 40,"O": 41,"P": 42,"Q":43,"R":44,"S":45,"T":46,"U":47,"V":48,"W":49,"X":50,"Y":51,"Z":52
 }
 
@@ -308,16 +308,40 @@ def addComma(x):
     return "{:,}".format(x)
 
 newLineSplits = inputData.split("\n")
-print(len(newLineSplits))
 totalValue = 0
 for rucksack in newLineSplits:
     compartment1, compartment2 = rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]
-    for i in range(len(compartment1)):
-        if compartment1[i] in compartment2[i]:
-            value = values[compartment1[i]]
+    for letter in compartment1:
+        if letter in compartment2:
+            value = values[letter]
             totalValue += value
             print("Added "+str(value)+" to the total!")
+            break
 
 print("="*10)
-# higher than 1,310
 print("Outcome value is: "+addComma(totalValue))
+print("="*10)
+print("Finding badge between the groups")
+
+def groupCheck(currentGroup):
+    print(currentGroup)
+    score = 0
+    for letter in currentGroup[0]:
+        if letter in currentGroup[1] and letter in currentGroup[2]:
+            score = values[letter]
+    print("Badge found in group for a score of "+str(score))
+    return score
+
+currentGroup = []
+totalScore = 0
+for rugsack in newLineSplits:
+    if len(currentGroup) >= 3:
+        totalScore += groupCheck(currentGroup)
+        currentGroup = []
+    currentGroup.append(rugsack)
+totalScore += groupCheck(currentGroup)
+
+# too high 25,305
+# too low 1,748
+# not 2,536
+print("Total score for the badges is: "+addComma(totalScore))
